9c159ef4efae19b818f26d2ad1dfa726bd71240c
Robert Defendini <robdefendini@gmail.com>
2020-05-28 23:21:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,9 @@
-
 require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key, value) final_array|
+    binding.pry 
+    final_array(key)
   binding.pry
 end
